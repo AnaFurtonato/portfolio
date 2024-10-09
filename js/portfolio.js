c4081355fb5d94071428d5c81d39be29e47c3e5f
AnaFurtonato <anafortunato44@gmail.com>
2024-10-09 16:37:55 -0300

    Img-Btn

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,33 +3,48 @@ document.addEventListener('DOMContentLoaded', function() {
   const sidebar = document.createElement('div');
   sidebar.classList.add('sidebar');
   sidebar.innerHTML = `
-    <div class="sidebar-content">
-      <a href="#" class="closebtn">&times;</a>
-      <ul class="menu-items">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Sobre</a></li>
-        <li><a href="#">Habilidades</a></li>
-        <li><a href="#">Projetos</a></li>
-        <li><a href="#">Contato</a></li>
-      </ul>
-      <a href="resume.pdf" class="botaoTopo">Entre em contato <img width="20" height="20" src="img/icons/icons-whatsapp.svg" alt="WhatsApp"></a>
-    </div>
+      <div class="sidebar-content">
+          <a href="#" class="closebtn">&times;</a>
+          <ul class="menu-items">
+              <!--<li><a href="#topo">Home</a></li>-->
+              <li><a href="#sobre">Sobre</a></li>
+              <li><a href="#habilidade">Habilidades</a></li>
+              <li><a href="#projetos">Projetos</a></li>
+              <li><a href="#contact">Contato</a></li>
+          </ul>
+          <a href="https://wa.me/11950645173" class="botaoTopo">Entre em contato <img width="20" height="20" src="img/icons/icons-whatsapp.svg" alt="WhatsApp"></a>
+      </div>
   `;
   document.body.appendChild(sidebar);
 
   const closeBtn = sidebar.querySelector('.closebtn');
 
   menuToggle.addEventListener('click', function() {
-    sidebar.style.width = "100%";
+      sidebar.style.width = "100%";
   });
 
   closeBtn.addEventListener('click', function() {
-    sidebar.style.width = "0";
+      sidebar.style.width = "0";
+  });
+
+  sidebar.querySelectorAll('.menu-items a').forEach(anchor => {
+      anchor.addEventListener('click', function(e) {
+          e.preventDefault(); 
+
+          const targetId = this.getAttribute('href');
+          const targetSection = document.querySelector(targetId);
+
+          targetSection.scrollIntoView({
+              behavior: 'smooth'
+          });
+
+          sidebar.style.width = "0";
+      });
   });
 
   window.addEventListener('resize', function() {
-    if (window.innerWidth >= 769) {
-      sidebar.style.width = "0";
-    }
+      if (window.innerWidth >= 769) {
+          sidebar.style.width = "0";
+      }
   });
 });
